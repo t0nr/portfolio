83eea856e6681b468d9568d28eec76516da48376
Ton Rattanasoontorn <trattanasoontorn@gmail.com>
2023-09-01 14:06:48 -0400

    removing resume

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -81,13 +81,6 @@ const Navbar = () => {
               Github <FaGithub size={30}/>
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f68]'>
-            <a className='flex justify-between items-center w-full text-white' 
-            href={resume} download="Resume">
-              Resume <BsFillPersonLinesFill size={30}/>
-            </a>
-          </li>
-          
         </ul>
       </div>
 
